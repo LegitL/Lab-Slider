706b7aa9e92a805dcb1848cc74e962c2c62ffec8
LegitL <luka.hinic@me.com>
2019-12-08 11:50:31 -0500

    Added logic for actual add, remove private/public and reorder. Using faker to generate mock data.

--- src/app/favorites/favorites.page.ts
+++ src/app/favorites/favorites.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit  } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
+import { ImagesService } from '../images.service';
 
 @Component({
   selector: 'app-favorites',
@@ -8,6 +9,7 @@ import { ModalController } from '@ionic/angular';
 })
 export class FavoritesPage implements OnInit {
   reorderMode = false;
+  items: any[];
 
   /**
    * ...
@@ -16,12 +18,15 @@ export class FavoritesPage implements OnInit {
    */
   constructor(
     private modalController: ModalController,
-  ) { }
+    private toastController: ToastController,
+    private imagesService: ImagesService,
+  ) {}
 
   /**
    * ...
    */
   public ngOnInit(): void {
+    this.items = this.imagesService.getAllItems();
   }
 
   /**
@@ -41,12 +46,76 @@ export class FavoritesPage implements OnInit {
   public doReorder(event: any): void {
     // The `from` and `to` properties contain the index of the item
     // when the drag started and ended, respectively
-    console.log(`Dragged from index ${event.detail.from} to ${event.detail.to}`);
+    this.imagesService.moveItem(event.detail.from, event.detail.to);
 
     // Finish the reorder and position the item in the DOM based on
     // where the gesture ended. This method can also be called directly
     // by the reorder group
     event.detail.complete();
+  }
+
+  /**
+   * ...
+   */
+  public add() {
+    const item = {
+      isWikiart: false,
+      isPublic: false,
+      title: '',
+      description: '',
+      url: `https://picsum.photos/seed/${Math.random()}/200/300`,
+    };
+    this.imagesService.addItem(item);
+  }
+
+  /**
+   * ...
+   *
+   * @param item ...
+   */
+  public async togglePublic(item: any) {
+    item.isPublic = !item.isPublic;
+    const toast = await this.toastController.create({
+      message: (item.isPublic) ? 'Item made public' : 'Item made private',
+      color: 'light',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  /**
+   * ...
+   *
+   * @param event ...
+   * @param index ...
+   */
+  public onTitleBlur(event: any, index: number): void {
+    const newTitle = event.target.value;
+    if (this.items[index].title !== newTitle) {
+      this.items[index].title = newTitle;
+    }
+  }
+
+  /**
+   * ...
+   *
+   * @param event ...
+   * @param index ...
+   */
+  public onDescriptionBlur(event: any, index: number): void {
+    const newDescription = event.target.value;
+    if (this.items[index].description !== newDescription) {
+      this.items[index].description = newDescription;
+    }
+  }
+
+  /**
+   * ...
+   *
+   * @param index ...
+   */
+  public delete(index: number): void {
+    this.imagesService.deleteItem(index);
   }
 
   /**
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,36 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { FavoritesPage } from '../favorites/favorites.page';
+import { ImagesService } from '../images.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
   slideOptions = {
     slidesPerView: 'auto',
     zoom: false,
   };
 
+  items: any[];
 
   /**
    * ...
    */
   public constructor(
-    private modalController: ModalController
+    private modalController: ModalController,
+    private imagesService: ImagesService
   ) {}
+
+  public ngOnInit(): void {
+    this.items = this.imagesService.getAllItems();
+  }
 
   /**
    * ...
-   * 
+   *
    * @param event ...
    */
   public onThumbnailTap(event) {
     const tappedIndex = event.target.swiper.clickedIndex;
     if (tappedIndex !== undefined) {
-      console.log(tappedIndex);
       if (tappedIndex === 0) {
         // 'more' has been tapped/clicked
         this.showMoreModal();
